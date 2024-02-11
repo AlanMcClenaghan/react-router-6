4576a23026d6f31a806b166c7219f0f2ea5f9d6b
Alan McClenaghan <AlanM@Alans-MBP-2.cust.communityfibre.co.uk>
2024-02-11 17:24:24 +0000

    Navigating, Nesting and Redirecting Routes

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -1,15 +1,27 @@
 import React from 'react';
+import { Route, Routes, NavLink, Navigate } from "react-router-dom";
+
+import HTML from './courses/HTML';
+import CSS from './courses/CSS';
+import JavaScript from './courses/JavaScript';
+
 
 const Courses = () => (
   <div className="main-content courses">
     <div className="course-header group">
       <h2>Courses</h2> 
       <ul className="course-nav">
-        <li><a href='#'>HTML</a></li>
-        <li><a href='#'>CSS</a></li>
-        <li><a href='#'>JavaScript</a></li>
+        <li><NavLink to='html'>HTML</NavLink></li>
+        <li><NavLink to='css'>CSS</NavLink></li>
+        <li><NavLink to='javaScript'>JavaScript</NavLink></li>
       </ul>
     </div>
+    <Routes>
+        <Route index element={<Navigate replace to="html" />} />
+        <Route path="html" element={<HTML />} />
+        <Route path="css" element={<CSS />} />
+        <Route path="javaScript" element={<JavaScript />} />
+      </Routes>
   </div>
 );
 
